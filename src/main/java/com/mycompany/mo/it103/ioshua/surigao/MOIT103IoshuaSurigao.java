20c0b673074ff548e1f373ef759fcc097ad48ce8
lrijsurigao-gif <lr.ijsurigao@mmdc.mcl.edu.ph>
2026-05-24 03:20:07 +0800

    First commit

---
+++ src/main/java/com/mycompany/mo/it103/ioshua/surigao/MOIT103IoshuaSurigao.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mo.it103.ioshua.surigao;
+
+import com.opencsv.CSVReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+/**
+ *
+ * @author Ioshua Jericho Surigao
+ */
+public class MOIT103IoshuaSurigao {
+
+    public static void main(String[] args) throws com.opencsv.exceptions.CsvValidationException {
+        // initialize the CSV files needed
+        String employeeCSV = "C:/Users/ioshs/Documents/NetBeansProjects/MO-IT101-Group34/src/main/java/com/mycompany/mo/it101/group34/MotorPH_Employee Data - Employee Details.csv";
+        String attendanceCSV = "C:/Users/ioshs/Documents/NetBeansProjects/MO-IT101-Group34/src/main/java/com/mycompany/mo/it101/group34/MotorPH_Employee Data - Attendance Record.csv";
+    }
+}
